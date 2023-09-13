bb9422bbdaf4d5a702a81de9f16874a4f2667781
Caio Alves <caioalves_diogo@hotmail.com>
2023-09-12 22:46:35 -0300

    Adicionar icones do font awesome e estilizações

--- frontend/src/components/HeaderNavegacao.jsx
+++ frontend/src/components/HeaderNavegacao.jsx
@@ -1,10 +1,14 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBasketShopping } from '@fortawesome/free-solid-svg-icons'
 import { Logo } from "./Logo"
 import './HeaderNavegacao.scss'
 
 export const HeaderNavegacao = () => (
         <header id="header-navegacao">
             <Logo />
-            <i id="header-navegacao__carrinho">C</i>
+            <FontAwesomeIcon
+                id="header-navegacao__carrinho"
+                icon={faBasketShopping} />
         </header>
     )
