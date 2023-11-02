c3091e075a7e4f09ebe9fde5da9e5144617d6a7c
Caio Alves <caioalves_diogo@hotmail.com>
2023-11-01 21:28:48 -0300

    Loading page de restaurante

--- frontend/src/components/ConteudoRestaurante.jsx
+++ frontend/src/components/ConteudoRestaurante.jsx
@@ -9,7 +9,8 @@ import { actions as restauranteActions } from "../actions/restaurante.actions"
 import { getValorEmReais } from "../utils"
 
 export const ConteudoRestaurante = () => {
-    const [restaurante, setRestaurante] = useState({ nome: '', catalogo: [], imagem: '', horarioAbertura: '', horarioFechamento: '', distancia: '' })
+    const [isLoading, setIsLoading] = useState(false)
+    const [restaurante, setRestaurante] = useState({ nome: 'Carregando...', catalogo: [], imagem: '', horarioAbertura: '...', horarioFechamento: '...', distancia: '0' })
     const dispatch = useDispatch()
     const idRestauranteCarrinho = useSelector(state => state.carrinhoReducers.carrinho.restaurante ? state.carrinhoReducers.carrinho.restaurante.id : undefined)
     const restaurantesCarregados = useSelector(state => state.restauranteReducers.restaurantes)
@@ -17,7 +18,10 @@ export const ConteudoRestaurante = () => {
     const idRestauranteParam = parseInt(idRestaurante.replaceAll(/\D/g,''))
 
     useEffect(() => {
-        api.restaurantes.getRestaurante(idRestauranteParam, true).then(restaurante => setRestaurante(restaurante))
+        setIsLoading(true)
+        api.restaurantes.getRestaurante(idRestauranteParam, true)
+            .then(restaurante => setRestaurante(restaurante))
+            .finally(() => setIsLoading(false))
     },[ idRestauranteParam ])
 
     const adicionarItemAoCarrinho = ({ item, restaurante }) => {
@@ -32,32 +36,34 @@ export const ConteudoRestaurante = () => {
 
     return (
         <main id="conteudo-restaurante">
-            { restaurante ? (
-                <>
-                    <div id="conteudo-restaurante__painel">
-                        <img id="conteudo-restaurante__painel__img" src={restaurante.imagem} alt={`Imagem do restaurante ${restaurante.nome}`}/>
-                        <h2 id="conteudo-restaurante__painel__titulo">{restaurante.nome }</h2>
+            {isLoading 
+                ? <div>Carregando...</div>
+                : restaurante ? (
+                    <>
+                        <div id="conteudo-restaurante__painel">
+                            <img id="conteudo-restaurante__painel__img" src={restaurante.imagem} alt={`Imagem do restaurante ${restaurante.nome}`}/>
+                            <h2 id="conteudo-restaurante__painel__titulo">{restaurante.nome }</h2>
+                        </div>
+                        <div id="conteudo-restaurante__catalogo">
+                            {restaurante.catalogo && ( restaurante.catalogo.length === 0 ? (
+                                <div>Nenhum item no catálogo :/</div>
+                            ) : restaurante.catalogo.map(item => (
+                                <CardItem
+                                    titulo={item.nome} 
+                                    valor={getValorEmReais(item.valor)}
+                                    subtitulos={item.detalhes}
+                                    onClick={() => adicionarItemAoCarrinho({item, restaurante})}
+                                    title={'Adicionar ao carrinho'}
+                                    key={item.id} />
+                            )))}
+                        </div>
+                    </>
+                ) : (
+                    <div id="item-nao-encontrado">
+                        <div>Restaurante não encontrado :/</div>
+                        <Link to={'/inicio'} id="voltar-inicio">Voltar ao início!</Link>
                     </div>
-                    <div id="conteudo-restaurante__catalogo">
-                        {restaurante.catalogo && ( restaurante.catalogo.length === 0 ? (
-                            <div>Nenhum item no catálogo :/</div>
-                        ) : restaurante.catalogo.map(item => (
-                            <CardItem
-                                titulo={item.nome} 
-                                valor={getValorEmReais(item.valor)}
-                                subtitulos={item.detalhes}
-                                onClick={() => adicionarItemAoCarrinho({item, restaurante})}
-                                title={'Adicionar ao carrinho'}
-                                key={item.id} />
-                        )))}
-                    </div>
-                </>
-            ) : (
-                <div id="item-nao-encontrado">
-                    <div>Restaurante não encontrado :/</div>
-                    <Link to={'/inicio'} id="voltar-inicio">Voltar ao início!</Link>
-                </div>
-            )}
+                )}
         </main>
     )
 }
